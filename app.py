a51d91a792864cdf68473d8b49e7e10a93c467b9
urvashi1921 <urvashitiwari1921@gmail.com>
2023-04-14 09:24:44 +0530

    changes in file

--- app.py
+++ app.py
@@ -5,10 +5,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def get_file():
-    # s3 = boto3.client('s3')
-    # response = s3.get_object(Bucket='zeeurvashi', Key='test_file.json')
-    # file_content = response['Body'].read().decode('utf-8')
-    return "Hello , World !"
+    s3 = boto3.client('s3')
+    response = s3.get_object(Bucket='zeeurvashi', Key='test_file.json')
+    file_content = response['Body'].read().decode('utf-8')
+    return file_content
 
 if __name__ == '__main__':
     app.run()
