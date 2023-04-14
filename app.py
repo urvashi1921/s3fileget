e80e85dee7ce11e30c8051e96189cea480f2ca36
urvashi1921 <urvashitiwari1921@gmail.com>
2023-04-14 08:26:05 +0530

    flask api runing

--- app.py
+++ app.py
@@ -5,10 +5,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def get_file():
-    s3 = boto3.client('s3')
-    response = s3.get_object(Bucket='zeeurvashi', Key='test_file.json')
-    file_content = response['Body'].read().decode('utf-8')
-    return file_content
+    # s3 = boto3.client('s3')
+    # response = s3.get_object(Bucket='zeeurvashi', Key='test_file.json')
+    # file_content = response['Body'].read().decode('utf-8')
+    return "Hello , World !"
 
 if __name__ == '__main__':
     app.run()
