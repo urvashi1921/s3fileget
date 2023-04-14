9a67852da195855e1c1867daa72375d76fd201b6
urvashi1921 <urvashitiwari1921@gmail.com>
2023-04-14 08:07:27 +0530

    flask api runing

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask
 import boto3
 
 app = Flask(__name__)
@@ -8,7 +8,7 @@ def get_file():
     s3 = boto3.client('s3')
     response = s3.get_object(Bucket='zeeurvashi', Key='test_file.json')
     file_content = response['Body'].read().decode('utf-8')
-    return "Hello world"
+    return file_content
 
 if __name__ == '__main__':
     app.run()
